b41cc24043eb7cb8e5727d6869764e5bd61092c5
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-12-21 12:02:07 +0600

    Full project update: JAMAL_PROJECT – new apps, templates, settings

--- pr/forms.py
+++ pr/forms.py
@@ -30,6 +30,7 @@ class PrForm(forms.ModelForm):
             'project_name_pr': forms.Select(attrs={'class': 'form-control'}),
             'requisition_date_pr': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'requisition_no': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter requisition number'}),
+            'resource_name_pr': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Enter resource'}),
             'resource_unit_pr': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Auto-fill unit'}),
             'quantity_pr': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter quantity'}),
             'delivery_date_pr': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
@@ -40,31 +41,27 @@ class PrForm(forms.ModelForm):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
-        # Whole-Project-Drop-down
-        # Empty label for-project-dropdown
+        # Project dropdown
         self.fields['project_name_pr'].empty_label = "--------- Select Project Name ---------"
-        # Filter-projects-by logged-in user
         if user:
             queryset = Project.objects.filter(created_by=user)
         else:
             queryset = Project.objects.none()
-        # Preserve selected-project-during edit
         if self.instance.pk and self.instance.project_name_pr:
             selected = Project.objects.filter(pk=self.instance.project_name_pr.pk)
             queryset = (queryset | selected).distinct()
         self.fields['project_name_pr'].queryset = queryset
 
-        # Whole-Resource-Drop-down
-        # Empty label for-resource-dropdown
+        # Resource dropdown
         self.fields['resource_name_pr'].empty_label = "--------- Select Resource Name ---------"
-        # Filter-resources-by logged-in user
         if user:
             queryset = Resource.objects.filter(created_by=user)
         else:
             queryset = Resource.objects.none()
-        # Preserve selected-resource-during edit
         if self.instance.pk and self.instance.resource_name_pr:
             selected = Resource.objects.filter(pk=self.instance.resource_name_pr.pk)
             queryset = (queryset | selected).distinct()
         self.fields['resource_name_pr'].queryset = queryset
+
+
 
